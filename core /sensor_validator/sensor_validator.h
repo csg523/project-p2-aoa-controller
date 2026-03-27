fe983a3db109cc87df4369840027ceb63d07e9b3
Aditya Ashok Pise <101938972+adipise5@users.noreply.github.com>
2026-03-27 19:30:18 +0530

    Update sensor_validator.h

--- core /sensor_validator/sensor_validator.h
+++ core /sensor_validator/sensor_validator.h
@@ -1,1 +1,20 @@
+#ifndef SENSOR_VALIDATOR_H
+#define SENSOR_VALIDATOR_H
 
+#include <stdint.h>
+
+#define NUM_SENSORS 3
+#define OUTLIER_THRESHOLD 5.0f   // configurable based on system
+
+typedef struct {
+    float value[NUM_SENSORS];
+} SensorInput_t;
+
+typedef struct {
+    float validated_values[NUM_SENSORS];
+    float weights[NUM_SENSORS];
+} SensorOutput_t;
+
+void SensorValidator_Process(SensorInput_t *input, SensorOutput_t *output);
+
+#endif
