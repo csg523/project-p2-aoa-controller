cf50e720d1926d013004cffad7a73bc9108852b2
Aditya Ashok Pise <101938972+adipise5@users.noreply.github.com>
2026-03-31 18:45:52 +0530

    Add Kalman filter structure and functions

--- core /aoa_estimator/aoa_estimator.h
+++ core /aoa_estimator/aoa_estimator.h
@@ -1,1 +1,14 @@
+#ifndef AOA_ESTIMATOR_H
+#define AOA_ESTIMATOR_H
 
+typedef struct {
+    float estimate;
+    float error_covariance;
+    float process_noise;
+    float measurement_noise;
+} KalmanState_t;
+
+void Kalman_Init(KalmanState_t *state);
+float Kalman_Update(KalmanState_t *state, float measurement);
+
+#endif
